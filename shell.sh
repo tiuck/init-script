88745821db0d767fa4c6d37a32008e02b1500fee
tiuck <runge@mailbox.org>
2015-11-27 21:09:46 +0100

    Debian-style network config

--- shell.sh
+++ shell.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 blue=`tput setaf 6`
+red= `tput setaf 1`
 reset=`tput sgr0`
 function e {
 echo ${blue} $1 ${reset}
+}
+function a {
+echo ${red} $1 ${reset}
 }
 
 
 
 cd ~
-e "Setup .vimrc..."
+a "Setup .vimrc..."
 echo "set nocompatible" > .vimrc
 echo "EDITOR=vi
 export EDITOR" >> .profile
 source .profile
-e "Setup .bashrc..."
+a "Setup .bashrc..."
 echo "mcd() { mkdir \"$1\"; cd \"$1\"; }
 mccd() { mkdir -P \"$1\"; cd \"$1\"; }
 alias ll='ls -ahl'
@@ -21,13 +25,13 @@ bak(){ mv \"$1\" \"$1.bak\"; cp \"$2\" \"$1\"; }" >> .bashrc
 source .bashrc
 
 
-e "Install...."
+a "Install...."
 sudo apt-get install ncdu \
 git \
 wget \
 unzip 
 
-e "Update system?"
+a "Update System?"
 e "1: sudo apt-get -y update"
 e "2: sudo yum -y update"
 read -p "3: do nothing " yn
@@ -36,8 +40,28 @@ case $yn in
  2) sudo yum -y update ;;
  *) ;;
 esac 
-e "Fuege Public Hostkey hinzu"
+e "Add Public SSH Key"
 mkdir .ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAimIvph2gT6wudQGlyWKIcP/m3RZa+mMzapdRQwTwSbdFtSl2LnL7NvMK4JDLweNc8KX3YyMyJQb0XeyHGWgG0Mc5Wd3rOAblW/uOHd6vzaHlDUFIldxwd9ibVShTIc/HAdKqlxqt8DbXN2pBLw75uFLvMaxCzfte7kyz9ALfF63xgWBqGKIC/6woFJWIuByvCZtDBN3gwf6scXQwtHWTBEXH3wpOf2+47faoKD1L7y0D7lYtBENkBYq7QJb9uUO7HMAa7d5WR5hPniwpySfg3Jp5AqM89ZVuQHfuzN4k/PpE1XnL2I1eNS7CntvU5l4lCnabF402ytGEhpNPybUVAQ== rsa-key-20151122" > .ssh/authorized_keys2
 chmod 700 .ssh
 chmod 600 .ssh/authorized_keys2
+
+a "Network Config"
+e "1: Debian-style (/etc/network/interfaces)"
+e "2: Redhat-style (/etc/sysconfig/network-scripts/ifcfg-ethX)"
+e "No network config"
+read -p "Choose wisely: " network-config
+case $network-config in
+	1)
+		e "1: add 192.168.1.112 (gateway 192.168.1.1) on eth0"
+		e "2: add 192.168.1.??? (gateway 192.168.1.1) on eth0"
+		e "3: manual config"
+		read -p "Choose wisely: " ip-change
+		case $ip-change in
+		 1) sudo apt-get update ;;
+		 2) sudo yum -y update ;;
+		 *) ;;
+		esac;;
+	
+		
+	esac
